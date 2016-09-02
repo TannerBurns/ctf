9c13222206c1f0c667b008fd2ef32f4990646340
Tanner Burns <tanner@tanner-mba.local>
2016-09-01 22:34:49 -0500

    Ctf

--- scripts/vagrant_setup.sh
+++ scripts/vagrant_setup.sh
@@ -22,9 +22,9 @@ apt-get -y install openjdk-7-jdk
 apt-get -y install ruby2.0
 apt-get -y install ruby2.0-dev
 #fix for running 32 bit binary on 64 bit system
-apt-get install lib32z1
-apt-get install lib32ncurses5
-apt-get install lib32bz2-1.0
+apt-get -y install lib32z1
+apt-get -y install lib32ncurses5
+apt-get -y install lib32bz2-1.0
 
 npm install -g coffee-script
 npm install -g react-tools
