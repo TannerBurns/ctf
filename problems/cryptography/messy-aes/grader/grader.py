6ccde784d9110893118d2c0cd132c68921975937
qijungu <qijungu@gmail.com>
2017-09-06 14:47:14 -0500

    2017 fall

--- problems/cryptography/messy-aes/grader/grader.py
+++ problems/cryptography/messy-aes/grader/grader.py
@@ -1,5 +1,5 @@
 def grade(arg, key):
-    if "FLAG{sorts_gross_spoon_programmers_exchangers_heap}" in key:
+    if "FLAG{looks_like_you_can_break_aes}" in key:
         return True, "Good job!"
     else:
         return False, "Nope, that's not the flag."
